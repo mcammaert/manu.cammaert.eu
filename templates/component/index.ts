efc4c64ab54b46c34d156a315434134edad48957
Manu Cammaert <manu@cammaert.eu>
2019-10-04 13:33:48 +0200

    Add index

---
+++ templates/component/index.ts
@@ -0,0 +1,6 @@
+import * as S from './COMPONENT_NAME.style';
+
+export { default as COMPONENT_NAMEContainer } from './COMPONENT_NAME.container';
+export { default as COMPONENT_NAME } from './COMPONENT_NAME';
+export * from './COMPONENT_NAME.types';
+export { S };
