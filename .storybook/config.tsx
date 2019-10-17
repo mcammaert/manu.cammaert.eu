797f47ee108065e23e7168c7b7b4a0af7b1fe198
Manu Cammaert <manu@cammaert.eu>
2019-10-17 23:15:23 +0200

    Fix storybook config

--- .storybook/config.tsx
+++ .storybook/config.tsx
@@ -1,9 +1,13 @@
-import { configure } from '@storybook/react';
+import { configure, addDecorator } from '@storybook/react';
+import { jsxDecorator } from 'storybook-addon-jsx';
+import { withKnobs } from '@storybook/addon-knobs';
+
+addDecorator(withKnobs);
+addDecorator(jsxDecorator);
 
 const req = require.context('../src', true, /\.stories\.tsx$/);
 
 function loadStories() {
-  console.info(req);
   req.keys().forEach(req);
 }
 
