fec83242d8d66b1a8423cab35396aa11c838a2a4
Manu Cammaert <manu@cammaert.eu>
2019-10-17 22:29:40 +0200

    Mute linting error

--- testing/react-testing-library/index.tsx
+++ testing/react-testing-library/index.tsx
@@ -2,6 +2,7 @@ import { render } from '@testing-library/react';
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const renderComponent = ({ theme, component }: any) => render(<ThemeProvider theme={theme}>{component}</ThemeProvider>);
 
 export default renderComponent;
