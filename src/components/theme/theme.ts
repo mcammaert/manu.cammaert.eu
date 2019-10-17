270e4e5b6162503b3b7cc619681052c4333d40a9
Manu Cammaert <manu@cammaert.eu>
2019-10-17 22:30:13 +0200

    Add global style

--- src/components/theme/theme.ts
+++ src/components/theme/theme.ts
@@ -1,3 +1,11 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+  body {
+    background-color: #ccc;
+  }
+`;
+
 const theme = {
   main: 'mediumseagreen',
 };
