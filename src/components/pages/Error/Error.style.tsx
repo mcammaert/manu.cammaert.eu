1776faafbc6e2a3a60c276cbf660160a8074bb7a
Manu Cammaert <manu@cammaert.eu>
2019-10-25 21:22:43 +0200

    Add title and message

--- src/components/pages/Error/Error.style.tsx
+++ src/components/pages/Error/Error.style.tsx
@@ -1,5 +1,22 @@
 import styled from 'styled-components';
+import { modularScale, rem } from 'polished';
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  padding-left: ${({ theme }) => rem(theme.margin)};
+  padding-right: ${({ theme }) => rem(theme.margin)};
+`;
 
 export const Error = styled.div``;
 
-export default { Error };
+export const Title = styled.p`
+  font-size: ${modularScale(1)};
+  margin-bottom: ${modularScale(0)};
+`;
+
+export const Message = styled.p`
+  font-size: ${modularScale(0)};
+`;
+
+export default { Container, Error, Title, Message };
--- src/components/pages/Error/Error.tsx
+++ src/components/pages/Error/Error.tsx
@@ -4,10 +4,13 @@ import { ErrorProps } from './Error.types';
 
 import S from './Error.style';
 
-const Error: React.FC<ErrorProps> = () => (
-  <S.Error>
-    <p>Error</p>
-  </S.Error>
+const Error: React.FC<ErrorProps> = ({ message, title }) => (
+  <S.Container>
+    <S.Error>
+      <S.Title>{title || 'Fout'}</S.Title>
+      <S.Message>{message || 'Er is een fout opgetreden'}</S.Message>
+    </S.Error>
+  </S.Container>
 );
 
 export default Error;
