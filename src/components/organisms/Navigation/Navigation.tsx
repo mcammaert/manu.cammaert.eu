85480b41b412ddd6e015a4425211f5aa54d3a75c
Manu Cammaert <manu@cammaert.eu>
2019-10-26 11:31:56 +0200

    Fix memory leak

--- src/components/organisms/Navigation/Navigation.tsx
+++ src/components/organisms/Navigation/Navigation.tsx
@@ -1,12 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 
 import { useLockBodyScroll } from 'react-use';
-import { interpolate, useTransition, animated, useTrail } from 'react-spring';
+import { interpolate, useTransition, animated, useTrail, ReactSpringHook, useChain } from 'react-spring';
 
 import { ScreenReaderOnly } from 'components/atoms/ScreenReaderOnly';
 import { NavigationProps } from './Navigation.types';
 
 import S from './Navigation.style';
+
+const links = [
+  {
+    title: 'Home',
+    uri: '/',
+  },
+  {
+    title: 'Curriculum Vitae',
+    uri: '/cv',
+  },
+  {
+    title: 'Contact',
+    uri: '/contact',
+  },
+];
 
 const Navigation: React.FC<NavigationProps> = () => {
   const [showNavigation, setShowNavigation] = useState(false);
@@ -17,44 +32,39 @@ const Navigation: React.FC<NavigationProps> = () => {
 
   useLockBodyScroll(showNavigation);
 
+  const basicRef = {
+    start: () => {},
+    stop: () => {},
+  };
+
+  const transRef = useRef<ReactSpringHook>(basicRef);
   const transitions = useTransition(showNavigation, null, {
     from: { position: 'absolute', opacity: 0, zIndex: 999, left: 0, top: 0, width: '100vw', height: '100vh' },
     enter: { opacity: 1 },
     leave: { opacity: 0 },
+    ref: transRef,
   });
 
-  const links = [
-    {
-      title: 'Home',
-      uri: '/',
-    },
-    {
-      title: 'Curriculum Vitae',
-      uri: '/cv',
-    },
-    {
-      title: 'Contact',
-      uri: '/contact',
-    },
-  ];
-
+  const trailRef = useRef<ReactSpringHook>(basicRef);
   const trail = useTrail(links.length, {
     opacity: showNavigation ? 1 : 0,
     x: showNavigation ? 0 : 20,
+    ref: trailRef,
   });
+
+  useChain(showNavigation ? [transRef, trailRef] : [trailRef, transRef]);
 
   const clickLinkHandler = () => {
     setShowNavigation(false);
   };
 
-  const AnimatedMenuItem = animated(({ title, uri, ...rest }: { title: string; uri: string; style: any }) => (
-    // eslint-disable-next-line react/jsx-props-no-spreading
-    <S.MenuItem {...rest}>
+  const MenuItem = ({ title, uri }: { title: string; uri: string }) => (
+    <S.MenuItem>
       <S.Link to={uri} onClick={clickLinkHandler}>
         {title}
       </S.Link>
     </S.MenuItem>
-  ));
+  );
 
   return (
     <S.NavigationContainer>
@@ -72,12 +82,13 @@ const Navigation: React.FC<NavigationProps> = () => {
                     {trail.map(({ x, ...rest }, index) => {
                       const { uri, title } = links[index];
                       return (
-                        <AnimatedMenuItem
+                        <animated.div
                           key={uri}
-                          uri={uri}
-                          title={title}
                           style={{ ...rest, transform: interpolate([x], (value: number) => `translate3d(0,${value}px,0)`) }}
-                        />
+                          // eslint-disable-next-line react/jsx-props-no-spreading
+                          {...rest}>
+                          <MenuItem uri={uri} title={title} />
+                        </animated.div>
                       );
                     })}
                   </S.Menu>
