7416597a8874a94c61fbaf5c8bbcd7df39591b26
Manu Cammaert <manu@cammaert.eu>
2019-10-27 18:48:48 +0100

    Add content

--- src/components/pages/Home/Home.test.tsx
+++ src/components/pages/Home/Home.test.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 
 import { cleanup } from '@testing-library/react';
 import 'jest-styled-components';
+import { BrowserRouter } from 'react-router-dom';
+import { advanceTo, clear } from 'jest-date-mock';
 
 import { renderComponent } from 'testing/react-testing-library';
 import { lightTheme } from 'components/theme';
@@ -13,9 +15,17 @@ afterEach(cleanup);
 describe('Home', () => {
   describe('default props', () => {
     it('should match the snapshot', () => {
-      const component = <Home />;
+      advanceTo(new Date(2018, 5, 27, 8, 0, 0)); // reset to date time.
+
+      const component = (
+        <BrowserRouter>
+          <Home />
+        </BrowserRouter>
+      );
       const { container } = renderComponent({ theme: lightTheme, component });
       expect(container).toMatchSnapshot();
+
+      clear();
     });
   });
 });
--- src/components/pages/Home/Home.tsx
+++ src/components/pages/Home/Home.tsx
@@ -1,20 +1,69 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+import { useInterval } from 'react-use';
+import { Link } from 'react-router-dom';
 
 import { Container } from 'components/atoms/Container';
+import { Copy } from 'components/atoms/Copy';
 import { Default } from 'components/templates/Default';
 
 import { HomeProps } from './Home.types';
 
 import S from './Home.style';
 
-const Home: React.FC<HomeProps> = () => (
-  <S.Home>
-    <Default>
-      <Container>
-        <p>Home</p>
-      </Container>
-    </Default>
-  </S.Home>
-);
+const getSalutation = (hour: number): string => {
+  if (hour <= 6) return 'Goede nacht';
+  if (hour <= 10) return 'Goede morgen';
+  if (hour <= 14) return 'Goede middag';
+  if (hour <= 17) return 'Goede namiddag';
+  if (hour <= 21) return 'Goede avond';
+  return 'Goede nacht';
+};
+
+const Home: React.FC<HomeProps> = () => {
+  const [hour, setHour] = useState(0);
+
+  useInterval(() => {
+    setHour(new Date(Date.now()).getHours());
+  }, 10000);
+
+  useEffect(() => {
+    setHour(new Date(Date.now()).getHours());
+  }, []);
+
+  return (
+    <S.Home>
+      <Default>
+        <Container>
+          <Copy margin size={1}>
+            {getSalutation(hour)}, ik ben Manu Cammaert, front-end developer / designer met een passie voor
+            <span> </span>
+            <abbr title="User Interface">UI</abbr>
+            <span> </span>
+            design en fotografie.
+          </Copy>
+          <Copy margin>
+            Mijn specialisatie ligt momenteel bij React, waarin ook deze website is gebouwd. Je kan de
+            <span> </span>
+            <a href="https://github.com/mcammaert/manu.cammaert.eu" aria-label="broncode van deze website">
+              broncode ervan
+            </a>
+            <span> </span>
+            hier bekijken.
+          </Copy>
+          <Copy>
+            Bekijk hier
+            <span> </span>
+            <Link to="/cv">
+              mijn <abbr title="curriculum vitae">cv</abbr>
+            </Link>
+            <span> </span>
+            of <Link to="/contact">contacteer me</Link> voor vragen.
+          </Copy>
+        </Container>
+      </Default>
+    </S.Home>
+  );
+};
 
 export default Home;
